c7b0365ab79b5ee67e69e3134b86d35a6c9c43b5
Taylor Welling <tw3080@gmail.com>
2016-07-05 23:11:35 -0700

    askQuestions() and getPreferences() are now methods of the Bartender object

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
 var questions = [];
 var ingredientList = [];
-var pantry;
 var preferences = {};
 var count;
 var drinkPrefs = $('#drink-prefs');
 var makeDrinkBtn = $('#make-drink-btn');
-/*
-var questionContainer = document.getElementById('question-container');
-var questionText = document.getElementById('question-text');
-var yesButton = document.getElementById('yes-button');
-var noButton = document.getElementById('no-button');
-var drinkContainer = document.getElementById('drink-container');
-*/
 
 // Question constructor
 function Question(type, question) {
@@ -36,28 +28,11 @@ function Ingredient(type, name) {
 
 // List of ingredients
 ingredientList = [
-  // Another way to make the ingredients list? Couldn't this just replace the pantry?
   new Ingredient('strong', ['rum', 'whiskey', 'gin']),
-  new Ingredient('salty', ['olives', 'salt rim']),
+  new Ingredient('salty', ['olives', 'salt rim', 'celery salt']),
   new Ingredient('bitter', ['bitters', 'tonic', 'lemon twist']),
   new Ingredient('sweet', ['simple syrup', 'honey', 'coke']),
   new Ingredient('fruity', ['orange slice', 'cassis', 'cherry'])
-  /*
-  new Ingredient('strong', 'rum'),
-  new Ingredient('strong', 'whiskey'),
-  new Ingredient('strong', 'gin'),
-  new Ingredient('salty', 'olives'),
-  new Ingredient('salty', 'salt rim'),
-  new Ingredient('bitter', 'bitters'),
-  new Ingredient('bitter', 'tonic'),
-  new Ingredient('bitter', 'lemon twist'),
-  new Ingredient('sweet', 'simple syrup'),
-  new Ingredient('sweet', 'honey'),
-  new Ingredient('sweet', 'coke'),
-  new Ingredient('fruity', 'orange slice'),
-  new Ingredient('fruity', 'cassis'),
-  new Ingredient('fruity', 'cherry')
-  */
 ];
 
 // Pantry constructor
@@ -65,8 +40,77 @@ function Pantry(ingredients) {
   this.ingredients = ingredients;
 }
 
+Pantry.prototype.getRandom = function(i) {
+  var numIngredients = this.ingredients[i].name.length;
+  var random = Math.floor(Math.random() * numIngredients);
+
+  console.log(numIngredients);
+};
+
+// Bartender constructor
+function Bartender(questions) {
+  this.questions = questions;
+}
+
+// Populate list of drink options
+Bartender.prototype.askQuestions = function() {
+  for (var i = 0; i < questions.length; i++) {
+    drinkPrefs.append('<input type="checkbox" id="box' + (i + 1) + '"/>' + questions[i].question + '<br/>');
+  }
+};
+
+// Get the user's drink type preferences based on which flavor boxes are checked
+Bartender.prototype.getPreferences = function() {
+  for (var i = 0; i < questions.length; i++) {
+    if ($('#box' + (i + 1)).prop('checked')) {
+      // console.log('box' + (i + 1) + ' checked');
+      preferences[questions[i].question] = true;
+    } else {
+      preferences[questions[i].question] = false;
+    }
+  }
+  console.log(preferences);
+};
+
+// Construct a drink made up of random ingredients from the user's preferences
+Bartender.prototype.makeDrink = function(preferences) {
+  for (var prop in preferences) {
+    if (preferences[prop]) {
+      console.log(preferences[prop]);
+    } else {
+      console.log(preferences[prop]);
+    }
+  }
+};
+
+// Initialize bartender
+var bartender = new Bartender(questions);
+
 // Pantry containing all ingredients
-pantry = new Pantry(ingredientList);
+var pantry = new Pantry(ingredientList);
+
+// Events
+makeDrinkBtn.on('click', function() {
+  bartender.getPreferences();
+  bartender.makeDrink(preferences);
+});
+
+// Execute on load
+$(function() {
+  // makeDrinkTry();
+  // console.log(pantry.ingredients[0].type);
+  // console.log(pantry.ingredients[0].name);
+  bartender.askQuestions();
+});
+
+// NOT REALLY USING THESE METHODS RIGHT NOW
+/*
+// Testing functionality of getRandom()
+function makeDrinkTry() {
+  for (var i = 0; i < questions.length; i++) {
+    pantry.getRandom(i);
+  }
+}
 
 // Populate list of drink options
 function init() {
@@ -74,58 +118,4 @@ function init() {
     drinkPrefs.append('<input type="checkbox" id="box' + (i + 1) + '"/>' + questions[i].question + '<br/>');
   }
 }
-
-$(function() {
-  init();
-  console.log(pantry);
-});
-
-// Events
-makeDrinkBtn.on('click', function() {
-  for (var i = 0; i < questions.length; i++) {
-    if ($('#box' + (i + 1)).prop('checked')) {
-      console.log('box' + (i + 1) + ' checked');
-      preferences[questions[i].question] = true;
-    } else {
-      preferences[questions[i].question] = false;
-    }
-  }
-  console.log(preferences);
-  /*
-  if ($('#box1').prop('checked')) {
-    preferences.
-    console.log('box1 checked');
-  }
-  if ($('#box2').prop('checked')) {
-    console.log('box2 checked');
-  }
-  if ($('#box3').prop('checked')) {
-    console.log('box3 checked');
-  }
-  if ($('#box4').prop('checked')) {
-    console.log('box4 checked');
-  }
-  if ($('#box5').prop('checked')) {
-    console.log('box5 checked');
-  }
-  */
-});
-
-/*
-// Events
-document.addEventListener('DOMContentLoaded', function() {
-  questionText.innerHTML += (questions[0].question);
-});
-
-yesButton.addEventListener('click', function() {
-  count = 0;
-  type = questions[count].type;
-  preferences[type] = 'yes';
-  console.log(preferences);
-});
-
-noButton.addEventlistener('click', function() {
-  count = 0;
-
-});
 */
